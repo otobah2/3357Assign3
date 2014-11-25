e8493d4b904ca6c55bc2d6499bc861183c779484
Antoine Alarie <aalarie2@uwo.ca>
2014-11-25 18:25:01 -0500

    simplified getopt code

--- client.c
+++ client.c
@@ -16,30 +16,20 @@
 
 int main(int argc, char** argv)
 {
-
   //create our input variables
   char* type = "A";
   char* dnsserver;
   char* query;
+  static struct option long_options[]=
+  {
+    {"type", required_argument, 0, 't'},
+    {0, 0, 0, 0}
+  };
 
   //handle inputs
-  int c;
-
-  while(1)
+  int option_index = 0, c;
+  while ((c = getopt_long(argc, argv, "t:", long_options, &option_index)) != -1)
   {
-    static struct option long_options[]=
-    {
-      {"type", required_argument, 0, 't'},
-      {0, 0, 0, 0}
-    };
-
-    int option_index = 0;
-    c = getopt_long(argc, argv, "t:", long_options, &option_index);
-
-    //if the options are all processed, stop iterating
-    if(c == -1)
-      break;
-
     //switch cases to handle options
     switch(c)
     {
@@ -52,29 +42,22 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
         break;
     }//end of switch statements
-
   }//end of input while loop
 
   //handle non-option arguments
   //
-  /*first if there are no arguments left, neither server nor query
-   *  were given
-   */
-  if(optind == argc)
+  /*if there are no arguments left, neither server nor query were given*/
+  if(optind == argc || optind == argc-1)
   {
-    printf("Error: Neither server nor query were given.\n");
-    printf("Expected Syntax: nsl [TYPE] [DNSSERVER] [QUERY]\n");
+    printf("Error: server or query not specified.\n");
+    printf("Expected Syntax: nsl [TYPE] DNSSERVER QUERY\n");
     exit(EXIT_FAILURE);
-  }else if(optind == argc-1)
+  }
+  else
   {
-    //if only one argument was provided, assume query ommitted
-    printf("Error: query was ommmitted.\n");
-    exit(EXIT_FAILURE);
-   }else
-   {
     dnsserver = argv[optind];
     query = argv[optind+1];
-   }
+  }
   
    //print options
    printf("Type       : %s\n", type);
